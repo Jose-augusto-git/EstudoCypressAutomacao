197daa50e9c37a16888e185d430306e77841a7f1
jose-augusto-git <jose.augusto.n.borges@gmail.com>
2025-02-26 14:16:35 -0300

    versao0.5

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: "9w9e6w",
+  projectId: "rufz94",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
