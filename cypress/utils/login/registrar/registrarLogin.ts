6844991049ab0c9c1cf87a1006081b6bee82498c
jose-augusto-git <jose.augusto.n.borges@gmail.com>
2025-02-28 14:08:45 -0300

    versao0.7

--- cypress/utils/login/registrar/registrarLogin.ts
+++ cypress/utils/login/registrar/registrarLogin.ts
@@ -1,9 +1,26 @@
+import { fakerDE as faker } from '@faker-js/faker';
+
 export function registroLogin(){
-    //cy.get('.ihdmxA').click();
 
-    
+    //campo email
+    cy.get('.kOeYBn, :nth-child(2) > .input__default').click({force:true});
+    cy.get('.kOeYBn, :nth-child(2) > .input__default').type(faker.internet.email());
 
+    //campo nome
+    cy.get(':nth-child(3) > .input__default').click({force:true});
+    cy.get(':nth-child(3) > .input__default').type(faker.internet.username());
 
+    //campo senha
+    cy.get(':nth-child(4) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').click({force:true});
+    cy.get(':nth-child(4) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('1234');
+
+    //confirmação da senha
+    cy.get(':nth-child(5) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').click({force:true});
+    cy.get(':nth-child(5) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('1234');
+
+    cy.get('.styles__ContainerFormRegister-sc-7fhc7g-0 > .style__ContainerButton-sc-1wsixal-0').click({force:true});
+
+    cy.get('.styles__ContainerInformations-sc-8zteav-3').should('be.visible');
 
 
 }
